696e4394217a908a516ab0ec63919f444acc414b
Ibrahim Ismail <ebrahemesmael@gmail.com>
2024-09-20 00:08:48 +0300

    .

--- proudcts.js
+++ proudcts.js
@@ -1,165 +1,249 @@
 export let products = [{
+        id: 1,
+        category: 'Bottoms',
+        gender: '',
         name: 'SATEEN CARGO PANT',
         priceCents: 22000,
         colors: 2,
         pic: 'pic1'
     }, {
+        id: 2,
+        category: '',
+        gender: '',
         name: 'KEYCHAIN',
         priceCents: 4000,
         colors: 1,
         pic: 'pic2'
     },
     {
+        id: 3,
+        category: '',
+        gender: '',
         name: 'SATEEN ANORAK',
         priceCents: 26000,
         colors: 2,
         pic: 'pic3'
     },
     {
+        id: 4,
+        category: '',
+        gender: '',
         name: 'DOVE HOODIE',
         priceCents: 24000,
         colors: 5,
         pic: 'pic4'
     },
     {
+        id: 5,
+        category: '',
+        gender: '',
         name: 'DOVE LONGSLEEVE TEE',
         priceCents: 14000,
         colors: 5,
         pic: 'pic5'
     },
     {
+        id: 6,
+        category: '',
+        gender: '',
         name: 'LOOSE TANK TOP',
         priceCents: 8000,
         colors: 2,
         pic: 'pic6'
     },
     {
+        id: 7,
+        category: '',
+        gender: '',
         name: 'DOVE 3/4 SLEEVE TEE',
         priceCents: 14000,
         colors: 5,
         pic: 'pic7'
     },
     {
+        id: 8,
+        category: '',
+        gender: '',
         name: 'SNAKE BAG',
         priceCents: 14000,
         colors: 1,
         pic: 'pic8'
     },
     {
+        id: 9,
+        category: '',
+        gender: '',
         name: 'LOGO NO SEAM TEE',
         priceCents: 12000,
         colors: 3,
         pic: 'pic9'
     },
     {
+        id: 10,
+        category: '',
+        gender: '',
         name: 'LOGO LONGSLEEVE TEE',
         priceCents: 12000,
         colors: 4,
         pic: 'pic10'
     },
     {
+        id: 11,
+        category: '',
+        gender: '',
         name: 'MENS SLEEVELESS SECOND SKIN',
         priceCents: 6000,
         colors: 2,
         pic: 'pic11'
     },
     {
+        id: 12,
+        category: '',
+        gender: '',
         name: 'DOVE SHRUNKEN HOODIE',
         priceCents: 24000,
         colors: 2,
         pic: 'pic12'
     },
     {
+        id: 13,
+        category: '',
+        gender: '',
         name: 'WOMENS TANK TOP SECOND SKIN',
         priceCents: 6000,
         colors: 2,
         pic: 'pic13'
     },
     {
+        id: 14,
+        category: '',
+        gender: '',
         name: 'MENS LONG SLEEVE SECOND SKIN',
         priceCents: 8000,
         colors: 2,
         pic: 'pic14'
     },
     {
+        id: 15,
+        category: '',
+        gender: '',
         name: 'WOMENS SLEEVELESS SECOND SKIN',
         priceCents: 6000,
         colors: 2,
         pic: 'pic15'
     },
     {
+        id: 16,
+        category: '',
+        gender: '',
         name: 'WOMENS LONG SLEEVE SECOND SKIN',
         priceCents: 8000,
         colors: 2,
         pic: 'pic16'
     },
     {
+        id: 17,
+        category: '',
+        gender: '',
         name: 'DOVE NO SEAM TEE',
         priceCents: 14000,
         colors: 4,
         pic: 'pic17'
     },
     {
+        id: 18,
+        category: '',
+        gender: '',
         name: 'NO SEAM TEE',
         priceCents: 12000,
         colors: 1,
         pic: 'pic18'
     },
     {
+        id: 19,
+        category: '',
+        gender: '',
         name: 'WOMENS LONG SLEEVE BODYSUIT',
         priceCents: 30000,
         colors: 1,
         pic: 'pic19'
     },
     {
+        id: 20,
+        category: '',
+        gender: '',
         name: 'LOGO 3/4 SLEEVE TEE',
         priceCents: 12000,
         colors: 5,
         pic: 'pic20'
     },
     {
+        id: 21,
+        category: '',
+        gender: '',
         name: 'SATEEN OVERALLS',
         priceCents: 30000,
         colors: 1,
         pic: 'pic21'
     },
     {
+        id: 22,
+        category: '',
+        gender: '',
         name: 'T CUT PARKA',
         priceCents: 34000,
         colors: 1,
         pic: 'pic22'
     },
     {
+        id: 23,
+        category: '',
+        gender: '',
         name: 'LOGO CAP',
         priceCents: 60000,
         colors: 1,
         pic: 'pic23'
     },
     {
+        id: 24,
+        category: '',
+        gender: '',
         name: 'COATED COTTON LIGHT PARKA',
         priceCents: 30000,
         colors: 1,
         pic: 'pic24'
     },
     {
+        id: 25,
+        category: '',
+        gender: '',
         name: 'FOLDABLE CAP',
         priceCents: 6000,
         colors: 1,
         pic: 'pic25'
     },
     {
+        id: 26,
+        category: 'Bottoms',
+        gender: '',
         name: 'WOMENS LONG LEGGING',
         priceCents: 14000,
         colors: 1,
         pic: 'pic26'
     },
     {
+        id: 27,
+        category: 'Bottoms',
+        gender: '',
         name: 'MENS LONG JOHN',
         priceCents: 8000,
         colors: 1,
         pic: 'pic27'
     },
     {
+        id: 28,
+        category: '',
+        gender: '',
         name: 'FLAME CAP',
         priceCents: 8000,
         colors: 1,
