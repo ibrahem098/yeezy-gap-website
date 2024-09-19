32fff747a0d8bef572f6b96203f97cd21faa387b
Ibrahim Ismail <ebrahemesmael@gmail.com>
2024-09-19 18:29:57 +0300

    scroll or horizontal products function added

--- script.js
+++ script.js
@@ -3,30 +3,49 @@ import {
 } from "/proudcts.js"
 let html = ``,
     picCount = 0
-products.forEach((product) => {
-    let productName = product.name,
-        productPriceCents = product.priceCents,
-        productcolors = product.colors,
-        productpic = product.pic
-    picCount++
 
-    html += `
-    <div class="product">
-    <img class="pic pic${picCount}" src="media/items/${picCount}a.jpg" onmouseover="this.src='media/items/${picCount}b.jpg'" onmouseout="this.src='media/items/${picCount}a.jpg'">
-    <p class="p_name">${productName}</p>
-    <p class="p_price">$${productPriceCents/100}</p>
-    <p class="p_color">${productcolors} COLORS</p>
-    </div>
-    `;
-    document.querySelector('.productsGrid').innerHTML = html
-})
-let heroimgNum = 0
-
-function generateeaderImg() {
+function generateHeaderImg() {
+    let heroimgNum;
     heroimgNum = Math.round(Math.random() * 16)
-    if (heroimgNum === 0) {
-        generateeaderImg()
+    if (heroimgNum < 1) {
+        heroimgNum++
     }
     document.querySelector('.headimg').style.backgroundImage = `url('media/heroimg/hero${heroimgNum}.jpg')`;
 }
-generateeaderImg()
+
+function generateProducts() {
+    products.forEach((product) => {
+        let productName = product.name,
+            productPriceCents = product.priceCents,
+            productcolors = product.colors,
+            productpic = product.pic
+        picCount++
+
+        html += `
+        <div class="product">
+        <img id="pic" class="pic pic${picCount}" src="media/items/${picCount}a.jpg" onmouseover="this.src='media/items/${picCount}b.jpg'" onmouseout="this.src='media/items/${picCount}a.jpg'">
+        <p class="p_name">${productName}</p>
+        <p class="p_price">$${productPriceCents/100}</p>
+        <p class="p_color">${productcolors} COLORS</p>
+        </div>
+        `;
+        document.querySelector('.productsGrid').innerHTML = html
+    })
+}
+
+
+generateHeaderImg()
+generateProducts()
+
+let scrollViewToggel = document.querySelector('.gridIcon');
+let grid = document.getElementById('productsGrid');
+
+scrollViewToggel.addEventListener('click', () => {
+    document.querySelectorAll('.pic').forEach((pic) => {
+        pic.classList.toggle("pic");
+        pic.classList.toggle("picScroll");
+    });
+    document.querySelector('.gridIcon').classList.toggle('gridGap');
+    grid.classList.toggle("productsGrid");
+    grid.classList.toggle("productsGridScroll");
+});
